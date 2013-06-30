f55e06f10444da8a87566f34657298ee7c11d135
Matthew Tiger <matthew@jetpackjoust.com>
2013-06-29 20:54:35 -0400

    added Article model class to /articles/models.py

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    """Model that represents an article to be inserted into
+    jetpackjoust.com/articles.
+    """
+    title = models.CharField("title of article", max_length=70,
+                             primarykey=True)
+    author = models.ForeignKey(Author, verbose_name="author of article")
+    content = models.TextField("body of article")
+    summary = models.TextField("summary of article")
+    category = models.CharField("sub category of article", max_length=20)
+    published = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+
