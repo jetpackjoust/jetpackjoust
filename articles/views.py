b6df1615c858246d936afbc73dd215c5eeaf6fc3
Matthew Tiger <matthew@jetpackjoust.com>
2013-07-06 16:43:56 -0400

    Fleshed out views in articles/views to receive correct articles objects by time period

--- articles/views.py
+++ articles/views.py
@@ -7,19 +7,31 @@ from articles.models import Article
 def archive_by_year(request, year):
     """Returns list of articles archived by year.
     """
-    return HttpResponse("This is jousting with jetpacks by year.")
+    articles = Article.objects.filter(published__year=year)
+    articles = articles.order_by('-published', 'title')[:10]
+    output = "\n".join([article.title for article in articles])
+    return HttpResponse(output)
 
 
 def archive_by_month(request, year, month):
     """Returns list of articles archived by month.
     """
-    return HttpResponse("This is jousting with jetpacks by month.")
+    articles = Article.objects.filter(published__year=year,
+                                      published__month=month)
+    articles = articles.order_by('-published', 'title')[:10]
+    output = "\n".join([article.title for article in articles])
+    return HttpResponse(output)
 
 
 def archive_by_day(request, year, month, day):
     """Returns list of articles archived by day.
     """
-    return HttpResponse("This is jousting with jetpacks by day.")
+    articles = Article.objects.filter(published__year=year,
+                                      published__month=month,
+                                      published__day=day)
+    articles = articles.order_by('-published', 'title')[:10]
+    output = "\n".join([article.title for article in articles])
+    return HttpResponse(output)
 
 
 def index(request):
