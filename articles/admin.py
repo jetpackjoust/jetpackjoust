e5046f87892acc2d4faf4de57dacb841be14549a
Matthew Tiger <matthew@jetpackjoust.com>
2013-08-13 19:56:46 -0400

    Added ImageAdminInline to articles.admin in order to be able to add images to an article on the same page.

--- articles/admin.py
+++ articles/admin.py
@@ -4,7 +4,11 @@ from articles.models import Category
 from articles.models import Article
 from articles.models import Image
 
+class ImageAdminInline(admin.TabularInline):
+    model = Image
+
 class ArticleAdmin(admin.ModelAdmin):
+    inlines = (ImageAdminInline, )
     prepopulated_fields = {'slug': ('title',)}
 
 admin.site.register(Author)
