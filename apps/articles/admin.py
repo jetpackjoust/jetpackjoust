5471f21ffd804fc168bf98c34c75318e3f76ddeb
Matthew Tiger <matthew@jetpackjoust.com>
2013-08-18 11:40:28 -0400

    Added function get_readonly_fields to ArticleAdmin in admin.py so as to make primary key 'title' readonly after article object has been created.

--- apps/articles/admin.py
+++ apps/articles/admin.py
@@ -4,11 +4,23 @@ from articles.models import Category
 from articles.models import Article
 from articles.models import Image
 
+
 class ImageAdminInline(admin.TabularInline):
     model = Image
 
+
 class ArticleAdmin(admin.ModelAdmin):
     inlines = (ImageAdminInline,)
+
+    def get_readonly_fields(self, req, obj=None):
+        """Makes primary key field 'title' read-only if this object
+        is edited, otherwise, it is not if object is newly created.
+        """
+        if obj:
+            return ('title',)
+        else:
+            return ()
+
 
 admin.site.register(Author)
 admin.site.register(Category)
