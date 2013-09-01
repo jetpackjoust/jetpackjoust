50b980dd599ad72c09c24473f9f30f3e855b3f23
Matthew Tiger <matthew.c.tiger@gmail.com>
2013-09-01 11:55:43 -0400

    Added index_tags and show_tag functions in articles.views and articles.urls

--- apps/articles/urls.py
+++ apps/articles/urls.py
@@ -5,13 +5,17 @@ from articles import views
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
 
-    url(r'(?P<year>\d{4})/$', views.archive_by_year, name='by_year'),
+    url(r'tags/', views.index_tags, name='index_tags'),
 
-    url(r'(?P<year>\d{4})/(?P<month>\d{2})/$', views.archive_by_month,
+    url(r'tags/(?P<tag_slug>[^/]+)$', views.show_tag, name='show_tag'),
+
+    url(r'(?P<year>\d{4})/$', views.index_by_year, name='by_year'),
+
+    url(r'(?P<year>\d{4})/(?P<month>\d{2})/$', views.index_by_month,
         name='by_month'),
 
     url(r'(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$',
-        views.archive_by_day, name='by_day'),
+        views.index_by_day, name='by_day'),
 
     url(r'\d{4}/\d{2}/\d{2}/(?P<slug>[^/]+)$',
         views.show_article, name='show_article'),
--- apps/articles/views.py
+++ apps/articles/views.py
@@ -3,41 +3,11 @@ from django.template import RequestContext, loader
 from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import get_object_or_404
 
-from articles.models import Article, Image
-
-
-def archive_by_year(request, year):
-    """Returns list of articles archived by year.
-    """
-    articles = Article.objects.get(published__year=year)
-    articles = articles.order_by('-published', 'title')[:10]
-    output = "\n".join([article.title for article in articles])
-    return HttpResponse(output)
-
-
-def archive_by_month(request, year, month):
-    """Returns list of articles archived by month.
-    """
-    articles = Article.objects.get(published__year=year,
-                                      published__month=month)
-    articles = articles.order_by('-published', 'title')[:10]
-    output = "\n".join([article.title for article in articles])
-    return HttpResponse(output)
-
-
-def archive_by_day(request, year, month, day):
-    """Returns list of articles archived by day.
-    """
-    articles = Article.objects.get(published__year=year,
-                                      published__month=month,
-                                      published__day=day)
-    articles = articles.order_by('-published', 'title')[:10]
-    output = "\n".join([article.title for article in articles])
-    return HttpResponse(output)
+from articles.models import Article, Image, TaggedArticle
 
 
 def index(request):
-    latest_articles = Article.objects.order_by('-published')[:10]
+    latest_articles = Article.objects.order_by('-published')
     template = loader.get_template('articles/index.html')
     context = RequestContext(request, {
             'latest_articles': latest_articles,
@@ -45,7 +15,47 @@ def index(request):
     return HttpResponse(template.render(context))
 
 
+def index_by_year(request, year):
+    """Returns list of articles archived by year.
+    """
+    articles = Article.objects.get(published__year=year)
+    articles = articles.order_by('-published', 'title')
+    output = "\n".join([article.title for article in articles])
+    return HttpResponse(output)
+
+
+def index_by_month(request, year, month):
+    """Returns list of articles archived by month.
+    """
+    articles = Article.objects.get(published__year=year,
+                                      published__month=month)
+    articles = articles.order_by('-published', 'title')
+    output = "\n".join([article.title for article in articles])
+    return HttpResponse(output)
+
+
+def index_by_day(request, year, month, day):
+    """Returns list of articles archived by day.
+    """
+    articles = Article.objects.get(published__year=year,
+                                      published__month=month,
+                                      published__day=day)
+    articles = articles.order_by('-published', 'title')
+    output = "\n".join([article.title for article in articles])
+    return HttpResponse(output)
+
+
+def index_tags(request):
+    """Returns list of all tags in database and links to said tags.
+    """
+    output = ""
+    return HttpResponse(output)
+
+
 def show_article(request, slug):
+    """Displays article that has the provided slug using the show_article
+    template.
+    """
     article =  get_object_or_404(Article, slug=slug)
 
     images = Image.objects.filter(article=article)
@@ -57,4 +67,12 @@ def show_article(request, slug):
             'tags': article.tags.all()
             })
     return HttpResponse(template.render(context))
+
+
+def show_tag(request, tag_slug):
+    """Displays list of links to articles that have the associated
+    tag_slug.
+    """
+    output = ""
+    return HttpResponse(output)
 
