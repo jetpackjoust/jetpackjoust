28b599e37678353bb2c2df2f5f8a1197e5a67f8f
jetpackjoust <matthew@jetpackjoust.com>
2014-02-18 00:08:21 -0500

    Converted all views to class-based views

--- apps/articles/urls.py
+++ apps/articles/urls.py
@@ -3,27 +3,28 @@ from django.conf.urls import patterns, include, url
 from articles import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.index_articles, name='index'),
+    url(r'^$', views.ArticleListView.as_view(), name='index'),
 
-    url(r'tags/$', views.index_tags, name='index_tags'),
+    url(r'tags/$', views.TagListView.as_view(), name='index_tags'),
 
-    url(r'tags/(?P<tag_slug>[^/]+)$', views.show_tag, name='show_tag'),
+    url(r'tags/(?P<tag_slug>[^/]+)$', views.TagDetailView.as_view(), name='show_tag'),
 
-    url(r'contributors/$', views.index_contributors,
+    url(r'contributors/$', views.AuthorListView.as_view(),
         name='index_contributors'),
 
-    url(r'contributors/(?P<contributor_slug>[^/]+)$', views.show_contributor,
-        name='show_contributor'),
+    url(r'contributors/(?P<contributor_slug>[^/]+)$',
+        views.AuthorDetailView.as_view(), name='show_contributor'),
 
-    url(r'(?P<year>\d{4})/$', views.index_articles, name='by_year'),
+    url(r'(?P<year>\d{4})/$', views.ArticleListView.as_view(),
+        name='by_year'),
 
-    url(r'(?P<year>\d{4})/(?P<month>\d{2})/$', views.index_articles,
+    url(r'(?P<year>\d{4})/(?P<month>\d{2})/$', views.ArticleListView.as_view(),
         name='by_month'),
 
     url(r'(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/$',
-        views.index_articles, name='by_day'),
+        views.ArticleListView.as_view(), name='by_day'),
 
     url(r'(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<slug>[^/]+)$',
-        views.show_article, name='show_article'),
+        views.ArticleDetailView.as_view(), name='show_article'),
 
                        )
--- apps/articles/views.py
+++ apps/articles/views.py
@@ -2,113 +2,68 @@ from django.http import HttpResponse
 from django.template import RequestContext, loader
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import get_object_or_404, get_list_or_404
+from django.views.generic import DetailView, ListView
 
-from articles.models import Author, Article, Image, TaggedArticle
+from articles.models import Article, Author, TaggedArticle
 from taggit.models import Tag
 
 from utils.paginator import DiggPaginator, get_page
 
 
-def index_articles(request, **kwargs):
-    """Returns list of articles archived by filtering parameters
-    passed in kwargs.
-    """
+class ArticleDetailView(DetailView):
+    model = Article
+    template_name = 'articles/show_article.html'
 
-    filter_parameters = {"published__{0}".format(key):
-                         int(kwargs[key]) for key in kwargs}
-
-    if filter_parameters:
-        articles = get_list_or_404(Article.objects.filter(**filter_parameters))
-    else:
-        articles = get_list_or_404(Article.objects.all())
-
-    articles = get_page(articles, request)
-
-    template = loader.get_template('articles/index_articles.html')
-    context = RequestContext(request, {
-            'articles': articles
-            })
-    return HttpResponse(template.render(context))
+    def get_context_data(self, **kwargs):
+        context = super(ArticleDetailView, self).get_context_data(**kwargs)
+        return context
 
 
-def index_contributors(request):
-    """Display links to all contributors on the site.
-    """
-    authors = get_list_or_404(Author.objects.all())
-    authors = get_page(authors, request)
+class ArticleListView(ListView):
+    model = Article
+    template_name = 'articles/index_articles.html'
 
-    template = loader.get_template('articles/index_contributors.html')
-    context = RequestContext(request, {
-            'authors': authors
-            })
-    return HttpResponse(template.render(context))
+    def get_context_data(self, **kwargs):
+        context = super(ArticleListView, self).get_context_data(**kwargs)
+        return context
 
 
-def index_tags(request):
-    """Returns list of all tags in database and links to said tags.
-    """
-    tag_list = get_list_or_404(Tag.objects.all().order_by('name'))
+class AuthorDetailView(DetailView):
+    model = Author
+    template_name = 'articles/index_contributors.html'
 
-    tags = []
-
-    for tag in tag_list:
-        tags.append({'name': tag.name,
-                     'url': '/'.join(['/articles', 'tags', tag.slug])})
-
-    template = loader.get_template('articles/index_tags.html')
-    context = RequestContext(request, {
-            'tags': tags,
-            })
-    return HttpResponse(template.render(context))
+    def get_context_data(self, **kwargs):
+        context = super(AuthorDetailView, self).get_context_data(**kwargs)
+        return context
 
 
-def show_article(request, **kwargs):
-    """Displays article that has the provided slug using the show_article
-    template.
-    """
+class AuthorListView(ListView):
+    model = Author
+    template_name = 'articles/index_contributors.html'
 
-    article =  get_object_or_404(Article, slug=kwargs['slug'])
-
-    article = Article.objects.article_title(**kwargs)
-
-    images = Image.objects.filter(article=article)
-
-    template = loader.get_template('articles/show_article.html')
-    context = RequestContext(request, {
-            'article': article,
-            'images': images
-            })
-    return HttpResponse(template.render(context))
+    def get_context_data(self, **kwargs):
+        context = super(AuthorListView, self).get_context_data(**kwargs)
+        print(context)
+        return context
 
 
-def show_contributor(request, contributor_slug):
-    """Displays all articles written by contributor as identified
-    by contributor_slug.
-    """
-    articles = get_list_or_404(Article.objects
-                               .filter(author__contributor_slug__iexact=
-                                       contributor_slug))
+class TagDetailView(DetailView):
+    model = Tag
+    template_name = 'articles/show_tag.html'
 
-    articles = get_page(articles, request)
-
-    template = loader.get_template('articles/index_articles.html')
-    context = RequestContext(request, {
-            'articles': articles
-            })
-    return HttpResponse(template.render(context))
+    def get_context_data(self, **kwargs):
+        context = super(TagDetailView, self).get_context_data(**kwargs)
+        print(context)
+        return context
 
 
-def show_tag(request, tag_slug):
-    """Displays list of links to articles that have the associated tag_slug.
-    """
-    articles = get_list_or_404(Article.objects.filter(tags__slug__iexact=
-                                                      tag_slug))
+class TagListView(ListView):
+    model = Tag
+    template_name = 'articles/index_tags.html'
 
-    articles = get_page(articles, request)
+    def get_context_data(self, **kwargs):
+        context = super(TagListView, self).get_context_data(**kwargs)
+        print(context)
+        return context
 
-    template = loader.get_template('articles/index_articles.html')
-    context = RequestContext(request, {
-            'articles': articles,
-            })
-    return HttpResponse(template.render(context))
 
