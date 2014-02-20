c29e7d47a5b2d0f2cffaa0366cfc971de658a8d3
jetpackjoust <matthew@jetpackjoust.com>
2014-02-19 21:33:13 -0500

    Revert "Added slug_field variable to detail views"
    This reverts commit e6b415c46746e9751e9e6df452c026366472bbb1.

--- apps/articles/views.py
+++ apps/articles/views.py
@@ -1,14 +1,18 @@
+from django.http import HttpResponse
+from django.template import RequestContext, loader
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.shortcuts import get_object_or_404, get_list_or_404
 from django.views.generic import DetailView, ListView
 
 from articles.models import Article, Author, TaggedArticle
 from taggit.models import Tag
 
+from utils.paginator import DiggPaginator, get_page
+
 
 class ArticleDetailView(DetailView):
     model = Article
     template_name = 'articles/show_article.html'
-    slug_field = 'slug'
 
     def get_context_data(self, **kwargs):
         context = super(ArticleDetailView, self).get_context_data(**kwargs)
@@ -27,10 +31,8 @@ class ArticleListView(ListView):
 class AuthorDetailView(DetailView):
     model = Author
     template_name = 'articles/index_contributors.html'
-    slug_field = 'contributor_slug'
 
     def get_context_data(self, **kwargs):
-        print(kwargs)
         context = super(AuthorDetailView, self).get_context_data(**kwargs)
         return context
 
@@ -47,7 +49,6 @@ class AuthorListView(ListView):
 class TagDetailView(DetailView):
     model = Tag
     template_name = 'articles/show_tag.html'
-    slug_field = 'slug'
 
     def get_context_data(self, **kwargs):
         context = super(TagDetailView, self).get_context_data(**kwargs)
