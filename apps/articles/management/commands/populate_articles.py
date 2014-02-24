7df81c2060e93c7b7039d7401ffb073180672ee1
jetpackjoust <matthew@jetpackjoust.com>
2014-02-23 23:47:47 -0500

    Rewrote populate_dev_db to populate_articles and it now creates articles for one article and two tags

--- apps/articles/management/commands/populate_articles.py
+++ apps/articles/management/commands/populate_articles.py
@@ -4,6 +4,11 @@ from optparse import make_option
 
 from articles.test_articles.test_models import TaggedArticleTwoTagsFactory
 from articles.test_articles.test_models import TaggedArticleThreeTagsFactory
+from articles.test_articles.test_models import AuthorFactory
+from articles.test_articles.test_models import TagFactory
+from articles.test_articles.test_models import ImageFactory
+
+
 
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
@@ -17,7 +22,16 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """Create tagged article model instance for number of times specified
-        in options['rows']
+        in options['rows'] for one author with same tags.
         """
-        for i in range(options['rows']):
-            TaggedArticleTwoTagsFactory()
+        rows = options['rows']
+
+        author = AuthorFactory()
+        tag_1 = TagFactory()
+        tag_2 = TagFactory()
+
+        for i in range(rows):
+            ImageFactory(article=TaggedArticleTwoTagsFactory(tag_1=tag_1,
+                                                             tag_2=tag_2,
+                                                             author=author))
+
